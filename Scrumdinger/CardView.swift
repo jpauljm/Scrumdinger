657b668e65ac3880bdabd90c79aae220968f3e40
Jean Paul Marinho <me@akanj.com>
2023-05-16 14:11:24 -0300

    Views
    Creating a card view and displaying data in a list completed

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,35 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Jean Paul on 15/05/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+            }
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static let scrum = DailyScrum.sampleData[0]
+    
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Jean Paul on 16/05/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+            
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
